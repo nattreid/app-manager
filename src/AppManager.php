09ab466825f389f5f3c9d8e7b59c1200707d00d4
Attreid <attreid@gmail.com>
2016-07-01 11:22:25 +0200

    prechod na dbal - uprava metod

--- src/AppManager.php
+++ src/AppManager.php
@@ -196,41 +196,44 @@ class AppManager {
      */
     public function backupDatabase() {
         $backup = new \NAttreid\Utils\TempFile('backup.sql', TRUE);
-        $tables = $this->db->query('SHOW TABLES')->fetchPairs();
+        $tables = $this->db->getPlatform()->getTables();
 
         $backup->write("SET NAMES utf8;\n");
-        $backup->write("SET time_zone = '+00:00';");
-        $backup->write("SET foreign_key_checks = 0;");
-        $backup->write("SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';");
+        $backup->write("SET time_zone = '+00:00';\n");
+        $backup->write("SET foreign_key_checks = 0;\n");
+        $backup->write("SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';\n\n\n");
 
         foreach ($tables as $table) {
-            $backup->write("DROP TABLE $table;\n");
+            $tableName = $table['name'];
+            $backup->write("DROP TABLE IF EXISTS `$tableName`;\n");
 
-            $createTable = $this->db->query("SHOW CREATE TABLE $table")->fetchField(1);
+            $createTable = $this->db->query("SHOW CREATE TABLE %table", $tableName)->fetch()->{'Create Table'};
             $backup->write("$createTable;\n\n");
 
-            $query = $this->db->query("SELECT * FROM $table");
-
-            $rows = $query->fetchAll();
-            $numColumn = $query->getColumnCount();
-
+            $rows = $this->db->query("SELECT * FROM %table", $tableName);
             $insert = [];
+            $columns = NULL;
             foreach ($rows as $row) {
-                $cols = [];
-                for ($col = 0; $col < $numColumn; $col++) {
-                    $row[$col] = addslashes($row[$col]);
-                    $row[$col] = preg_replace("/\n/", "\\n", $row[$col]);
-                    if (isset($row[$col])) {
-                        $cols[] = '"' . $row[$col] . '"';
-                    } else {
-                        $cols[] .= '""';
+                $field = $row->toArray();
+
+                if ($columns === NULL) {
+                    $colName = [];
+                    foreach ($field as $key => $value) {
+                        $colName[] = "`$key`";
                     }
+                    $columns = implode(', ', $colName);
                 }
-                $insert[] = implode(',', $cols);
+                $cols = [];
+                foreach ($field as $column) {
+                    $column = addslashes($column);
+                    $column = preg_replace("/\n/", "\\n", $column);
+                    $cols[] = '"' . $column . '"';
+                }
+                $insert[] = implode(', ', $cols);
             }
 
             if (!empty($insert)) {
-                $backup->write("INSERT INTO $table VALUES\n(" . implode("),\n(", $insert) . ");\n");
+                $backup->write("INSERT INTO `$tableName` ($columns) VALUES\n(" . implode("),\n(", $insert) . ");\n");
             }
 
             $backup->write("\n\n");
@@ -247,33 +250,62 @@ class AppManager {
      * Smaze vsechny tabulky v databazi
      */
     public function dropDatabase() {
-        $tables = $this->db->query('SHOW TABLES')->fetchPairs();
+        $tables = $this->db->getPlatform()->getTables();
         if (!empty($tables)) {
             $this->db->query('SET foreign_key_checks = 0');
-            $this->db->query('DROP TABLE ' . implode(',', $tables));
+            foreach ($tables as $table) {
+                $this->db->query('DROP TABLE %table', $table['name']);
+            }
             $this->db->query('SET foreign_key_checks = 1');
         }
-        $this->db->getStructure()->rebuild();
     }
 
     /**
      * Nahraje databazi
      * @param string $file
-     * @return boolean
+     * @author Jakub Vrána, Jan Tvrdík, Michael Moravec
+     * @license Apache License
      */
     public function loadDatabase($file) {
-        $this->db->beginTransaction();
-        try {
-            $this->db->query('SET foreign_key_checks = 0');
-            \Nette\Database\Helpers::loadFromFile($this->db->getConnection(), $file);
-            $this->db->query('SET foreign_key_checks = 1');
-            $this->db->commit();
-            return TRUE;
-        } catch (\PDOException $ex) {
-            Debugger::log($ex, \Tracy\ILogger::ERROR);
-            $this->db->rollBack();
-        }
-        return FALSE;
+        $this->db->transactional(function(Connection $db) use($file) {
+            $db->query('SET foreign_key_checks = 0');
+            $query = file_get_contents($file);
+
+            $delimiter = ';';
+            $offset = 0;
+            while ($query != '') {
+                if (!$offset && preg_match('~^\\s*DELIMITER\\s+(.+)~i', $query, $match)) {
+                    $delimiter = $match[1];
+                    $query = substr($query, strlen($match[0]));
+                } else {
+                    preg_match('(' . preg_quote($delimiter) . '|[\'`"]|/\\*|-- |#|$)', $query, $match, PREG_OFFSET_CAPTURE, $offset); // should always match
+                    $found = $match[0][0];
+                    $offset = $match[0][1] + strlen($found);
+
+                    if (!$found && rtrim($query) === '') {
+                        break;
+                    }
+
+                    if (!$found || $found == $delimiter) { // end of a query
+                        $q = substr($query, 0, $match[0][1]);
+
+                        $db->query($q);
+
+                        $query = substr($query, $offset);
+                        $offset = 0;
+                    } else { // find matching quote or comment end
+                        while (preg_match('~' . ($found == '/*' ? '\\*/' : (preg_match('~-- |#~', $found) ? "\n" : "$found|\\\\.")) . '|$~s', $query, $match, PREG_OFFSET_CAPTURE, $offset)) { //! respect sql_mode NO_BACKSLASH_ESCAPES
+                            $s = $match[0][0];
+                            $offset = $match[0][1] + strlen($s);
+                            if ($s[0] !== '\\') {
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+            $db->query('SET foreign_key_checks = 1');
+        });
     }
 
 }
