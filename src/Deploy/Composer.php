54392d3505d8f6684cb8642a885e1217f48031bd
Attreid <attreid@gmail.com>
2016-08-29 11:02:05 +0200

    update composer

--- src/Deploy/Composer.php
+++ src/Deploy/Composer.php
@@ -35,20 +35,33 @@ class Composer extends Deploy {
      */
     private function getComposer() {
         $composer = $this->tempDir . $this->composer;
+
+        $temp = $this->tempDir . $this->composerDir;
+        if (!file_exists($temp)) {
+            mkdir($temp);
+        }
+
+        $home = 'COMPOSER_HOME="' . $temp . '" ';
+
         if (!file_exists($composer)) {
+            $expectedSignature = trim(file_get_contents('https://composer.github.io/installer.sig'));
+            copy('https://getcomposer.org/installer', $this->tempDir . '/composer-setup.php');
+            $actualSignature = hash_file('SHA384', $this->tempDir . '/composer-setup.php');
+            if ($expectedSignature === $actualSignature) {
             $command = 'cd ' . $this->tempDir . ';'
-                    . 'php -r "copy(\'https://getcomposer.org/installer\', \'composer-setup.php\');";'
-                    . 'php -r "if (hash_file(\'SHA384\', \'composer-setup.php\') === \'e115a8dc7871f15d853148a7fbac7da27d6c0030b848d9b3dc09e2a0388afed865e6a3d6b3c0fad45c48e2b5fc1196ae\') { echo \'Installer verified\'; } else { echo \'Installer corrupt\'; unlink(\'composer-setup.php\'); } echo PHP_EOL;";'
-                    . 'php composer-setup.php;'
-                    . 'php -r "unlink(\'composer-setup.php\');";';
+                    . $home . 'php composer-setup.php;';
             exec($command, $output);
             foreach ($output as $str) {
                 if (!empty($str)) {
                     Debugger::log($str, 'composer');
                 }
             }
+                unlink($this->tempDir . '/composer-setup.php');
+            } else {
+                Debugger::log('Invalid installer signature', 'composer');
+            }
         }
-        return $composer;
+        return $home . $composer;
     }
 
     /**
@@ -57,13 +70,9 @@ class Composer extends Deploy {
     public function update() {
         $composer = $this->getComposer();
 
-        $temp = $this->temp . $this->composerDir;
-        if (!file_exists($temp)) {
-            mkdir($temp);
-        }
         $command = 'cd ' . $this->path . ';'
-                . 'COMPOSER_HOME="' . $temp . '" ' . $composer . ' self-update 2>&1;'
-                . 'COMPOSER_HOME="' . $temp . '" ' . $composer . ' update 2>&1;';
+                . $composer . ' self-update 2>&1;'
+                . $composer . ' update 2>&1;';
         exec($command, $output);
         foreach ($output as $str) {
             if (!empty($str)) {
