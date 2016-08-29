cdf3c135c3902fe0fee52803c9e656e0f85709ff
Attreid <attreid@gmail.com>
2016-08-29 10:12:22 +0200

    update composer

--- src/Deploy/Composer.php
+++ src/Deploy/Composer.php
@@ -37,8 +37,8 @@ class Composer extends Deploy {
         $composer = $this->tempDir . $this->composer;
         if (!file_exists($composer)) {
             $command = 'cd ' . $this->tempDir . ';'
-                    . 'php -r "readfile(\'https://getcomposer.org/installer\');" > composer-setup.php;'
-                    . 'php -r "if (hash_file(\'SHA384\', \'composer-setup.php\') === \'a52be7b8724e47499b039d53415953cc3d5b459b9d9c0308301f867921c19efc623b81dfef8fc2be194a5cf56945d223\') { echo \'Installer verified\'; } else { echo \'Installer corrupt\'; unlink(\'composer-setup.php\'); } echo PHP_EOL;";'
+                    . 'php -r "copy(\'https://getcomposer.org/installer\', \'composer-setup.php\');";'
+                    . 'php -r "if (hash_file(\'SHA384\', \'composer-setup.php\') === \'e115a8dc7871f15d853148a7fbac7da27d6c0030b848d9b3dc09e2a0388afed865e6a3d6b3c0fad45c48e2b5fc1196ae\') { echo \'Installer verified\'; } else { echo \'Installer corrupt\'; unlink(\'composer-setup.php\'); } echo PHP_EOL;";'
                     . 'php composer-setup.php;'
                     . 'php -r "unlink(\'composer-setup.php\');";';
             exec($command, $output);
