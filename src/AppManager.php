46a57b05ee28921456c9c1f11cee377751ece6b5
attreid <attreid@gmail.com>
2016-10-25 09:53:56 +0200

    uprava loadDatabase

--- src/AppManager.php
+++ src/AppManager.php
@@ -9,6 +9,7 @@ use NAttreid\Utils\File;
 use NAttreid\Utils\TempFile;
 use Nette\Utils\Finder;
 use Nextras\Dbal\Connection;
+use Nextras\Dbal\Utils\FileImporter;
 
 /**
  * Sprava aplikace
@@ -297,48 +298,12 @@ class AppManager
 	/**
 	 * Nahraje databazi
 	 * @param string $file
-	 * @author Jakub Vrána, Jan Tvrdík, Michael Moravec
-	 * @license Apache License
 	 */
 	public function loadDatabase($file)
 	{
 		$this->db->transactional(function (Connection $db) use ($file) {
 			$db->query('SET foreign_key_checks = 0');
-			$query = file_get_contents($file);
-
-			$delimiter = ';';
-			$offset = 0;
-			while ($query != '') {
-				if (!$offset && preg_match('~^\\s*DELIMITER\\s+(.+)~i', $query, $match)) {
-					$delimiter = $match[1];
-					$query = substr($query, strlen($match[0]));
-				} else {
-					preg_match('(' . preg_quote($delimiter) . '|[\'`"]|/\\*|-- |#|$)', $query, $match, PREG_OFFSET_CAPTURE, $offset); // should always match
-					$found = $match[0][0];
-					$offset = $match[0][1] + strlen($found);
-
-					if (!$found && rtrim($query) === '') {
-						break;
-					}
-
-					if (!$found || $found == $delimiter) { // end of a query
-						$q = substr($query, 0, $match[0][1]);
-
-						$db->query($q);
-
-						$query = substr($query, $offset);
-						$offset = 0;
-					} else { // find matching quote or comment end
-						while (preg_match('~' . ($found == '/*' ? '\\*/' : (preg_match('~-- |#~', $found) ? "\n" : "$found|\\\\.")) . '|$~s', $query, $match, PREG_OFFSET_CAPTURE, $offset)) { //! respect sql_mode NO_BACKSLASH_ESCAPES
-							$s = $match[0][0];
-							$offset = $match[0][1] + strlen($s);
-							if ($s[0] !== '\\') {
-								break;
-							}
-						}
-					}
-				}
-			}
+			FileImporter::executeFile($db, $file);
 			$db->query('SET foreign_key_checks = 1');
 		});
 	}
